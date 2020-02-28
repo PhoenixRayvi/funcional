0e6df690790dea168eab9630d5265dcbf6bf888b
rayrisson <rayrissonvinicius@gmail.com>
2020-02-28 12:38:10 -0300

    intervalo de lista entre n e m

---
+++ intervalo.hs
@@ -0,0 +1,6 @@
+rmMiolo list = putStrLn $ "" ++ show (list !! 0) ++ " " ++ show(list !! (length (list) - 1))
+
+intervalo list pointA pointB = 
+    if pointB > pointA
+        then putStrLn $ show [x | x <- list, x > pointA, x < pointB]
+        else putStrLn $ show [x | x <- list, x > pointB, x < pointA]
