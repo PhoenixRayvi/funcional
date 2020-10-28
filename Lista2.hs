5e37a6edaa36ded1db9c7e16c64d4e0a6e1c5075
Rayrisson <rayrissonvinicius@gmail.com>
2020-10-28 18:02:02 -0300

    Lista2

--- Lista2.hs
+++ Lista2.hs
@@ -14,27 +14,35 @@ swap list i1 i2 =
         then take i1 list ++ list !! i2 : take (i2 - 1) (drop (i1 + 1) list) ++ list !! i1 : drop (i2 + 1) list
         else swap list i2 i1
 
+nextPerm' [] list2 i = -1
+nextPerm' list1 list2 i
+    |list2 == head list1 = i
+    |otherwise = nextPerm' (tail list1) list2 (i+1)
+nextPerm list = (allPerms list) !! ((nextPerm' (allPerms list) list 0) + 1)
+
+allPerms [] = [[]]
+allPerms list = [x:y | x <- list, y <- (allPerms (filter (\a -> a /= x) list))] 
+
 buscaBin' i [] num = -1
 buscaBin' i list num
     | num == head list = i
     | otherwise = buscaBin' (i + 1) (tail list) num
 buscaBin list num = buscaBin' 0 list num
 
-{-
-listPrimes [] = []
-listPrimes list = 
-    if length [head list | x <- [2..100], head list `mod` x == 0] == 1
-        then head list : listPrimes (tail list)
-        else listPrimes (tail list)
+primos [] = []
+primos list = head list : primos [x | x <- list, x `mod` (head list) /= 0]
+formTuples [] = []
+formTuples list = (head xs, length xs) : formTuples ys
+                where xs = filter (== minimum list) list
+                      ys = filter (/= minimum list) list
+factors' 1 list = []
+factors' num list
+    | num `mod` (head list) == 0 = head list : factors' (num `div` (head list)) list 
+    | otherwise = factors' num (tail list)
+factors num = formTuples (factors' num (primos [2..]))
 
-biggerNum [x] = x
-biggerNum list = 
-    if head list > list !! 1
-        then biggerNum (head list : drop 2 list)
-        else biggerNum (tail list)
+listacc list = map (sum) [take x list | x <- [1 .. (length list)]]
 
---nextPerm list = 
---swap list num1 num2 = take num1 list ++ list !! num2 : drop (num1 + 1) list
--}
-
---allPerms list = [[x, y, z] | x <- list, y <- tail list, z <- drop 2 list]
+maxsseq' [] = []
+maxsseq' list = [take x list | x <- [1 .. (length list)]] ++ maxsseq' (tail list)
+maxsseq list = head [x | x <- (maxsseq' list),  sum (x) == (maximum (map (sum) (maxsseq' list)))]
